86a517c8307e1b46f2e1435af0dcf790fe1d3c64
Zane Rankin <zwrankin@gmail.com>
2020-01-20 21:15:34 -0600

    Fix setup.py (#16)

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import find_packages, setup
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
+    requirements.reverse  # need to ensure this order is same as requirements.txt
 
 setup(
     name='porygon',
