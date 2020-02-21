1340afadca88b9bd0e150ab9409737eed1b4b9b1
Zane Rankin <zwrankin@gmail.com>
2020-02-20 22:33:43 -0600

    remove utils module and git import (#22)

--- porygon/utils/__init__.py
+++ porygon/utils/__init__.py
@@ -1,3 +1,2 @@
-from porygon.utils.utils import PROJECT_DIR
 from porygon.utils.data import _validate_point_data, df_to_gpdf, gpdf_to_latlong_df
 from porygon.utils.voronoi import coords_to_voronoi_polygons
